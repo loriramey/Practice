5a89744bb25cb1280b04a0f3f01d5c6557b65fc5
Lori Ramey <loriramey@hotmail.com>
2022-08-06 16:09:20 -0400

    Added user input commands

--- radians_calc.py
+++ radians_calc.py
@@ -1,10 +1,14 @@
-#Calculator to convert between degrees and radians in angle measurements
+#Calculator to convert between degrees and radians
 #Lori Ramey, August 2022
 
 from cmath import pi
 
-input_unit = "radians"
-input_value = 2*pi/3
+input_unit = input("Please enter either 'degrees' or 'radians' and hit Enter:\n")
+print(f"You entered {input_unit}")
+input_value = input("Please enter the value you would like to convert and hit Enter:\n")
+input_value = int(float(input_value))
+print(f"You entered {input_value}")
+
 result = 0
 
 #function to convert between rad and deg
@@ -12,7 +16,7 @@ def convert(input_unit, input_value):
     
     if input_unit == "degrees":
        result  = input_value * pi / 180
-       print(str(rount(result,2)) + " radians")
+       print(str(round(result,2)) + " radians")
     else: 
        result = input_value * 180 / pi
        print(str(round(result,2)) + " degrees")
